f614493828a125afaba247acd29aa74432bc65d4
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-12-17 19:49:45 -0500

    Added build to docs for github pages test

---
+++ docs/precache-manifest.10ec8907c770c8ed2cd5f95a7e09c16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6420e24ef843860d266e6331d4cf0a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "926955b99abee121dec7",
+    "url": "/static/css/main.63392721.chunk.css"
+  },
+  {
+    "revision": "3b126493f6497b762ea9",
+    "url": "/static/js/2.0748b48f.chunk.js"
+  },
+  {
+    "revision": "926955b99abee121dec7",
+    "url": "/static/js/main.f962694f.chunk.js"
+  },
+  {
+    "revision": "427262b6771d3f49a7c5",
+    "url": "/static/js/runtime-main.4aea9da3.js"
+  },
+  {
+    "revision": "677e8c7ad89569a9ccaa462328327663",
+    "url": "/static/media/background.677e8c7a.jpg"
+  }
+]);
