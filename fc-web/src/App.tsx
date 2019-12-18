7a9cc0071da2f1ad8bf014622cb532f349e0abb1
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-12-18 07:28:22 -0500

    Google sign on test

--- fc-web/src/App.tsx
+++ fc-web/src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import CalendarView from "./CalendarView";
 import { observer } from "mobx-react";
 import CalendarState from "./State/CalendarState";
+import { GoogleLogin } from "react-google-login";
 
 const App: React.FC = observer(() => {
   if (CalendarState.calendar.id === "__BLANK__"){
@@ -10,9 +11,20 @@ const App: React.FC = observer(() => {
       <div>Loading...</div>
     );
   }
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  }
+  
   return (
     <div className="App" id="app">
-      <div className="content"><CalendarView/></div>
+      <GoogleLogin
+        clientId="214138073904-0hrvbipnivru968l2pr099dl3hf197iu.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+  />,
+      {/* <div className="content"><CalendarView/></div> */}
     </div>
   );
 });
